d5c51a138a5f0cc96e7818637f3963ea73b048d0
Arnau Marcos Almansa <arnaumarcosalmansa@gmail.com>
2023-09-29 11:40:00 +0200

    training addons as decorators

--- src/models/trainer.py
+++ src/models/trainer.py
@@ -5,26 +5,47 @@ import torch
 
 from src.config import device
 from src.dataloaders.SateliteDataLoader import PositionalEncode
+from abc import ABC, abstractmethod
 
 
-class Trainer:
-    def __init__(self, model, optimizer, loss, train_loader, test_loader, checkpoint=None, validation=None):
-        self.model = model
-        self.optimizer = optimizer
-        self.loss = loss
+class AbstractTrainer(ABC):
+
+    @abstractmethod
+    def train(self, epochs):
+        pass
+
+    @abstractmethod
+    def train_one_epoch(self, epoch):
+        pass
+
+    @abstractmethod
+    def model(self):
+        pass
+
+    @abstractmethod
+    def loss(self):
+        pass
+
+    @abstractmethod
+    def optimizer(self):
+        pass
+
+    @abstractmethod
+    def name(self):
+        pass
+
+
+class Trainer(AbstractTrainer):
+    def __init__(self, model, optimizer, loss, train_loader, name):
+        self._model = model
+        self._optimizer = optimizer
+        self._loss = loss
         self.train_loader = train_loader
-        self.test_loader = test_loader
-        self.checkpoint = checkpoint
-        self.validation = validation
+        self._name = name
 
     def train(self, epochs):
         for epoch in range(epochs):
             loss = self.train_one_epoch(epoch)
-            print(f"EPOCH {epoch}, train_loss = {loss:.5f}")
-            if self.checkpoint and self.checkpoint.should_do_checkpoint(epoch):
-                self.checkpoint.checkpoint(epoch, self.model, self.optimizer, loss)
-            if self.validation and self.validation.should_do_validation(epoch):
-                self.validation.validate(self.model, self.loss, self.test_loader)
 
     def train_one_epoch(self, epoch):
         running_loss = 0.
@@ -37,28 +58,42 @@ class Trainer:
             inputs, labels = data
 
             # Zero your gradients for every batch!
-            self.optimizer.zero_grad()
+            self._optimizer.zero_grad()
 
             # Make predictions for this batch
-            outputs = self.model(inputs).squeeze()
+            outputs = self._model(inputs).squeeze()
 
             # Compute the loss and its gradients
-            loss = self.loss(outputs, labels)
+            loss = self._loss(outputs, labels)
             loss.backward()
 
             # Adjust learning weights
-            self.optimizer.step()
+            self._optimizer.step()
 
             # Gather data and report
             running_loss += loss.item()
 
+        print(f"EPOCH {epoch}, train_loss = {running_loss / l:.5f}")
+
         return running_loss / l
+
+    def model(self):
+        return self._model
+
+    def loss(self):
+        return self._loss
+
+    def optimizer(self):
+        return self._optimizer
+
+    def name(self):
+        return self._name
 
 
 class RenderLossTrainer(Trainer):
 
-    def __init__(self, model, optimizer, loss, train_loader, test_loader, *, height, width, **kwargs):
-        super().__init__(model, optimizer, loss, train_loader, test_loader, **kwargs)
+    def __init__(self, model, optimizer, loss, train_loader, name, *, height, width):
+        super().__init__(model, optimizer, loss, train_loader, name)
         self.height = height
         self.width = width
 
@@ -69,23 +104,25 @@ class RenderLossTrainer(Trainer):
             timestamp, image = data
 
             # Zero your gradients for every batch!
-            self.optimizer.zero_grad()
+            self._optimizer.zero_grad()
 
             print(f"Rendering {i + 1}/{l}")
-            outputs = self.render(self.model, timestamp)
+            outputs = self.render(self._model, timestamp)
 
             print(f"Computing loss {i + 1}/{l}")
             # Compute the loss and its gradients
-            loss = self.loss(outputs, image.squeeze())
+            loss = self._loss(outputs, image.squeeze())
             loss.backward()
 
             # Adjust learning weights
-            self.optimizer.step()
+            self._optimizer.step()
 
             # Gather data and report
             running_loss += loss.item()
 
             print(f"Done {i + 1}/{l}")
+
+        print(f"EPOCH {epoch}, train_loss = {running_loss / l:.5f}")
 
         return running_loss / l
 
@@ -101,6 +138,31 @@ class RenderLossTrainer(Trainer):
                 im[i, j, :] = (model(se.do_positional_encoding(q)))
 
         return im
+
+
+class TrainerDecorator(AbstractTrainer):
+
+    def __init__(self, trainer: AbstractTrainer):
+        self._trainer = trainer
+
+    def train(self, epochs):
+        for epoch in range(epochs):
+            self.train_one_epoch(epoch)
+
+    def train_one_epoch(self, epoch):
+        self._trainer.train_one_epoch(epoch)
+
+    def model(self):
+        return self._trainer.model()
+
+    def loss(self):
+        return self._trainer.loss()
+
+    def optimizer(self):
+        return self._trainer.optimizer()
+
+    def name(self):
+        return self._trainer.name()
 
 
 class CheckPoint:
@@ -121,9 +183,72 @@ class CheckPoint:
         }, self.base_path + f"checkpoint_epoch_{epoch}.ckpt")
 
 
-class Validation:
+class Checkpoint(TrainerDecorator):
+    def __init__(self, trainer: AbstractTrainer, base_path):
+        super().__init__(trainer)
+        self.base_path = base_path + self.name() + "/"
+        self.epoch = 0
+        if not os.path.exists(self.base_path):
+            os.makedirs(self.base_path)
+        else:
+            self.load_latest_checkpoint()
+
+    def train(self, epochs):
+        epoch = 1
+        try:
+            for epoch in range(self.epoch, epochs):
+                self.train_one_epoch(epoch)
+        except KeyboardInterrupt as e:
+            self.checkpoint(epoch - 1, self.model(), self.optimizer(), self.loss())
+
+    def train_one_epoch(self, epoch):
+        self.epoch = epoch
+        self._trainer.train_one_epoch(epoch)
+        if self.should_do_checkpoint(epoch):
+            self.checkpoint(epoch, self.model(), self.optimizer(), self.loss())
+
+    def should_do_checkpoint(self, epoch) -> bool:
+        return epoch % 10 == 9
+
+    def checkpoint(self, epoch, model, optimizer, loss):
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': loss,
+        }, self.base_path + f"checkpoint_epoch_{epoch:010d}.ckpt")
+
+    def load_latest_checkpoint(self):
+        checkpoint_filenames = [self.base_path + filename for filename in os.listdir(self.base_path) if filename.endswith(".ckpt")]
+        if len(checkpoint_filenames) == 0:
+            return
+
+        last_checkpoint = list(sorted(checkpoint_filenames))[-1]
+
+        self.load_checkpoint(last_checkpoint)
+
+    def load_checkpoint(self, path):
+        checkpoint = torch.load(path)
+        self.model().load_state_dict(checkpoint['model_state_dict'])
+        self.optimizer().load_state_dict(checkpoint['optimizer_state_dict'])
+        self.epoch = checkpoint['epoch']
+
+
+class Validation(TrainerDecorator):
+    def __init__(self, trainer: AbstractTrainer, data_loader):
+        super().__init__(trainer)
+        self._data_loader = data_loader
+
+    def train_one_epoch(self, epoch):
+        self._trainer.train_one_epoch(epoch)
+        if self.should_do_validation(epoch):
+            self.do_validation()
+
     def should_do_validation(self, epoch) -> bool:
         return epoch % 10 == 9
+
+    def do_validation(self):
+        self.validate(self.model(), self.loss(), self._data_loader)
 
     def validate(self, model, loss_fn, data_loader):
         running_loss = 0.0
